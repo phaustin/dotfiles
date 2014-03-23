02d4fa912ba49d291422b86623319c5e373653ee
Phil Austin <paustin@eos.ubc.ca>
2014-03-23 11:45:47 -0700

    move to dotfiles_common

--- dot_emacs_d/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - rail at <2014-03-20 Thu 08:30>
-(setq stored-clock-history '(("/Users/phil/Dropbox/org/admin.org" . 762) ("/Users/phil/Dropbox/org/admin.org" . 763) ("/Users/phil/Dropbox/org/refile.org" . 2480) ("/Users/phil/Dropbox/org/refile.org" . 2513) ("/Users/phil/Dropbox/org/refile.org" . 2540)))
